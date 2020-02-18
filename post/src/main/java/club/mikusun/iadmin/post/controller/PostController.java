132c30af2517192978f40b1a8d7812fd2db7c534
Tico <1690552430@qq.com>
2020-02-18 18:36:01 +0800

    "初始化POST模块"

---
+++ post/src/main/java/club/mikusun/iadmin/post/controller/PostController.java
@@ -0,0 +1,7 @@
+package club.mikusun.iadmin.post.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PostController {
+}
---
+++ post/src/main/java/club/mikusun/iadmin/post/service/TokenService.java
@@ -0,0 +1,6 @@
+package club.mikusun.iadmin.post.service;
+
+import club.mikusun.iadmin.spring.shiro.interfaces.ShiroTokenService;
+
+public interface TokenService extends ShiroTokenService {
+}
